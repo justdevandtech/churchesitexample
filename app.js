f6bfd43c6c1aec1bdd596890167e425236dddb21
ophicial-pauloski <pasukwo5@gmail.com>
2020-08-11 12:50:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/*alert('The page you are about to visit is not the real website of the name you will see here. This is just an example of how a church website should looks like')
+*/
+
+
+const nocon1 = document.getElementById("nocon1");
+const nocon2 = document.getElementById("nocon2");
+const nocon3 = document.getElementById("nocon3");
+const nocon4 = document.getElementById("nocon4");
+const nocon5 = document.getElementById("nocon5");
+const nocon6 = document.getElementById("nocon6");
+
+
+
+nocon1.onclick = function (){
+    alert("No content...This not a fully functional website! please come back later and check. pauloski loves you")
+ };
+
+ nocon2.onclick = function (){
+    alert("No content...This not a fully functional website! please come back later and check. pauloski loves you")
+ };
+
+ nocon3.onclick = function (){
+    alert("No content...This not a fully functional website! please come back later and check. pauloski loves you")
+ };
+
+ nocon4.onclick = function (){
+    alert("No content...This not a fully functional website! please come back later and check. pauloski loves you")
+ };
+
+ nocon5.onclick = function (){
+    alert("No content...This not a fully functional website! please come back later and check. pauloski loves you")
+ };
+
+ nocon6.onclick = function (){
+    alert("This not a full functionally website! please come back later and check. pauloski loves you")
+ };
